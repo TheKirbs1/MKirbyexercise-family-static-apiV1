b562e4b1bbeb0341fecf2ff75218763f330abf1e
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-05-30 19:28:29 +0000

    All test passed

--- src/app.py
+++ src/app.py
@@ -11,9 +11,18 @@ from datastructures import FamilyStructure
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
-
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
 
 jackson_family.add_member(
     {
@@ -38,41 +47,53 @@ jackson_family.add_member(
     'lucky_numbers': [1]
     }
 )
+# #POST
+# @app.route('/members', methods=['POST'])
+# def add_new_member():
+#     new_family_member = request.json
+#     jackson_family.add_member(new_family_member)
+#     members = jackson_family.get_all_members()
+#     response_body = {
+#         "results": members
+#     }
+#     return jsonify(response_body), 200
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
-
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+#POST
+@app.route('/member', methods=['POST'])
+def create_member():
+    body = request.get_json()
+    new_member = {
+        "id": body["id"],
+        "first_name": body["first_name"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
-
-
-    return jsonify(response_body['family']), 200
-
-
-@app.route('/members', methods=['POST'])
-def add_new_member():
-    new_family_member = request.json
-    jackson_family.add_member(new_family_member)
-    members = jackson_family.get_all_members()
+    jackson_family.add_member(new_member)
     response_body = {
-        "results": members
+        "msg": "New member successfully added",
+        "member":  new_member
     }
-
     return jsonify(response_body), 200
+
+#GET members
+@app.route('/members', methods=['GET'])
+def get_members():
+
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
+
+#GET by ID
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+#Delete member by ID
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done" : True, "deleted_member": member}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
